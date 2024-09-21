cf72f10f4091a8480e6834cd602ec4b81cbec65d
GonnaFlyMethod <alex.husakou@gmail.com>
2024-09-21 12:42:09 +0200

    prepare/read-spir-v-shaders

--- Utilities.h
+++ Utilities.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <fstream>
+
 const std::vector<const char*> deviceExtensions = {
 	VK_KHR_SWAPCHAIN_EXTENSION_NAME
 };
@@ -25,3 +27,28 @@ struct SwapchainImage {
 	VkImage image;
 	VkImageView imageView;
 };
+
+static std::vector<char> readFile(const std::string& filename) {
+	// Open stream from given file
+	// std::ios::ate tells to start reading from end of file
+	std::ifstream file(filename, std::ios::binary | std::ios::ate);
+
+	// Check if file stream  successfully opened
+	if (!file.is_open()) {
+		throw std::runtime_error("error occurred while opening a file");
+	}
+
+	// Get current read position and use to resize file buffer
+	size_t fileSize = (size_t)file.tellg();
+	std::vector<char> fileBuffer(fileSize);
+
+	// Move read position (seek to) the start of the file
+	file.seekg(0);
+
+	// Read the file data into the buffer (stream "filesize" in total)
+	file.read(fileBuffer.data(), fileSize);
+
+	file.close();
+
+	return fileBuffer;
+}
